1e020fcbca92571bb6c1c4e54670d7e25d406f9a
shanin18 <112709243+shanin18@users.noreply.github.com>
2023-05-02 16:18:19 +0600

    added all chef

--- src/Pages/Chef/Chef.jsx
+++ src/Pages/Chef/Chef.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
 import ChefCard from "./ChefCard";
 
 const Chef = () => {
@@ -14,9 +13,12 @@ const Chef = () => {
 
   return (
     <div className="my-20">
-      <h2 className="text-4xl font-bold font-montserrat text-center mb-10">
+      <div className="flex justify-center">
+      <h2 className="text-3xl md:text-4xl font-bold font-montserrat mb-10 border-0 border-b-4 border-b-yellow-300 rounded-xl shadow-xl px-5 text-center mx-2 border-t py-4 w-fit">
         Find Your Favorite Chef
       </h2>
+      </div>
+
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-6">
         {allChef?.map((chef) => (
           <ChefCard key={chef.id} chef={chef}></ChefCard>
